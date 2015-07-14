18582e0882fff04aa563ea92d48f6711f475e3ce
gpechim <gpechim@gmail.com>
2015-07-14 17:44:33 -0300

    Adds google chart lib

--- .tmp/scripts/ggcharts.js
+++ .tmp/scripts/ggcharts.js
@@ -1,26 +1,32 @@
-// jshint devel:true
-// Google Chart
 "use strict";
 
-google.load("visualization", "1", { packages: ["corechart"] });
-google.setOnLoadCallback(drawChart);
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([["Element", "Density", { role: "style" }], ["Copper", 8.94, "#b87333"], ["Silver", 10.49, "silver"], ["Gold", 19.30, "gold"], ["Platinum", 21.45, "color: #e5e4e2"]]);
+google.load("visualization", "1.1", { packages: ["bar"] });
+google.setOnLoadCallback(drawStuff);
 
-  var view = new google.visualization.DataView(data);
-  view.setColumns([0, 1, { calc: "stringify",
-    sourceColumn: 1,
-    type: "string",
-    role: "annotation" }, 2]);
+// jsonData = $.ajax({
+//   url: "scripts/data-monthly.json",
+//   dataType:"json",
+//   async: false
+// }).responseText;
+
+function drawStuff() {
+  var data = new google.visualization.arrayToDataTable([["Period", "Rides"], ["2014-12", 5.000], ["2015-01", 12.600], ["2015-02", 22.273], ["2015-03", 16.478], ["2015-04", 18.129], ["2015-05", 20.102], ["2015-06", 12.812]]);
 
   var options = {
-    title: "Density of Precious Metals, in g/cm^3",
-    width: 600,
-    height: 400,
-    bar: { groupWidth: "95%" },
-    legend: { position: "none" }
+    // title: 'Chess opening moves',
+    width: "100%",
+    legend: { position: "none" },
+    // chart: { subtitle: 'popularity by percentage' },
+    // axes: {
+    //   x: {
+    //     0: { side: 'top', label: 'White to move'}
+    //   }
+    // },
+    bar: { groupWidth: "90%" }
   };
-  var chart = new google.visualization.ColumnChart(document.getElementById("ggCharts"));
-  chart.draw(view, options);
-}
+
+  var chart = new google.charts.Bar(document.getElementById("chart"));
+  // Convert the Classic options to Material options.
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+};
 //# sourceMappingURL=ggcharts.js.map
