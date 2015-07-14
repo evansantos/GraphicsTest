aaad8e3f11baaccd27996049b85d8b58f6a54266
Evandro Santos <evandro.santos@easytaxi.com.br>
2015-07-14 11:55:51 -0300

    Initial Project Commit

---
+++ app/scripts/d3-main.js
@@ -0,0 +1,17 @@
+// jshint devel:true
+// D3.JS Chart
+
+var width = 640, height = 480; // Measures for graph
+
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .1), //
+    y = d3.scale.linear().range([0, height]); //
+
+var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+    yAxis = d3.svg.axis().scale(y).orient("left");
+
+var svg = d3.select("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+
+
---
+++ app/scripts/main.js
@@ -0,0 +1,4 @@
+// jshint devel:true
+// D3.JS Chart
+
+var width = 640, height = 480; // Measures for graph
