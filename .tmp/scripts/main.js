2df3b9e61b20050399a2d00e3d2973e2190a05ea
Evandro Santos <evandro.santos@easytaxi.com.br>
2015-07-14 19:02:12 -0300

    refactor bar graph

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,8 +1,4 @@
-// jshint devel:true
-// D3.JS Chart
-
 "use strict";
 
-var width = 640,
-    height = 480; // Measures for graph
+// jshint devel:true
 //# sourceMappingURL=main.js.map
--- app/scripts/d3-main.js
+++ app/scripts/d3-main.js
@@ -2,8 +2,8 @@
 // D3.JS Chart
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 800 - margin.left - margin.right,
+    height = 480 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], 0.1);
@@ -54,7 +54,62 @@ d3.json("scripts/data-monthly.json", function(error, data) {
       .attr("x", function(d) { return x(d.date); })
       .attr("width", x.rangeBand())
       .attr("y", function(d) {
-        return  Math.abs(y(d.rides)) - (height + margin.top + margin.bottom);
+        return height - y(d.rides);
       })
-      .attr("height", function(d) { return Math.abs(y(d.rides)) - (height + margin.top + margin.bottom); });
+      .attr("height", function(d) {
+        return height - y(d.rides);
+      });
+});
+
+////////////
+var width = 420,
+    barHeight = 50;
+
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var chart = d3.select(".chart2")
+    .attr("width", width);
+
+
+d3.json("scripts/data.json", function(error, data) {
+  if (error) throw error;
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, width]);
+ x.domain([0, d3.max(data, function(d) { return d.taxiTaken; })]);
+
+  chart.attr("height", barHeight * data.length);
+
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  bar.append("rect")
+      .attr("width", function(d) { return x(d.taxiTaken); })
+      .attr("height", barHeight - 1);
+
+  bar.append("text")
+      .attr("x", 50)
+      .attr("y", 20)
+      .attr("class", "name")
+      .attr("dy", ".35em")
+      .html(function(d) { return d.name; });
+
+  bar.append("text")
+      .attr("x", 0)
+      .attr("y", 35)
+      .attr("class", "email")
+      .attr("dy", ".35em")
+      .html(function(d) { return d.email; });
+
+  bar.append("text")
+      .attr("x", function(d) { return x(d.taxiTaken)+ 30;})
+      .attr("y", barHeight/2)
+      .attr("class", "counter")
+      .attr("dy", ".35em")
+      .attr("stroke-width","60")
+      .html(function(d) { return d.taxiTaken; });
 });
